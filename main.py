cc8f2b8f5677d9803d321be8aa38b31b1f88250b
Claudio Sicari <claudiosicari99@gmail.com>
2022-01-03 20:41:30 +0100

    adding model logic for response and adding last API for settings

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import csv
+import datetime
+import re
 import uuid
 from typing import List
 
 import motor.motor_asyncio
 from bson import ObjectId
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 
-from models import BetData
+from models import BetData, BanUser, LimitResearches, Toggle, UserBanOut, UserLimitOut, ToggleOut
 
 app = FastAPI()
 client = motor.motor_asyncio.AsyncIOMotorClient(
@@ -42,9 +44,104 @@ async def parsing_to_csv(bet_data: List[BetData]):
 
 
 @app.get("/stats")
+# le = 'less equal' - ge = 'greater equal' (between 1 and 4 included)
 async def calculating_stats(stat: int = Query(..., le=4, ge=1)):
     match stat:
         case 1:
-            users = db['web_server_user'].find(projection={'_id': False, 'username': True})
-            usernames = [doc['username'] async for doc in users]
-            return {'names': usernames, 'count': len(usernames)}
+            users = await db['web_server_user'].find(projection={'_id': False, 'username': True}).to_list(None)
+            return {'users': users, 'count': len(users)}
+        case 2:
+            return await db['web_server_search'].count_documents({})
+        case 3:
+            researches_gb = await db['web_server_search'].count_documents({'web_site': 'goldbet'})
+            researches_bw = await db['web_server_search'].count_documents({'web_site': 'bwin'})
+            return {'goldbet': researches_gb, 'bwin': researches_bw}
+        case 4:
+            researches_count = await db['web_server_search'].count_documents({})
+            users_count = await db['web_server_user'].count_documents({})
+            if users_count == 0:
+                return 'empty'
+            users = await db['web_server_user'].aggregate([
+                {
+                    '$lookup': {
+                        'from': 'web_server_search',
+                        'localField': 'user_identifier',
+                        'foreignField': 'user_id',
+                        'as': 'search'
+                    }
+                },
+                {
+                    '$project': {
+                        '_id': False,
+                        'username': True,
+                        'count': {'$size': '$search'}
+                    }
+                }
+            ]).to_list(None)
+
+            return {'average': researches_count / users_count, 'users': users}
+
+
+@app.post("/ban", response_model=UserBanOut)
+async def ban_user(ban_user: BanUser):
+    def parser_date(date):
+        if date == 'perma':
+            return datetime.datetime.max
+        period = re.search(r'(\d)y(\d)m(\d)d', date)
+        years = int(period.group(1))
+        months = int(period.group(2))
+        days = int(period.group(3))
+        return datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=days + months * 30 + years * 365)
+
+    if ban_user.period == 'perma':
+        period = parser_date('perma')
+    elif ban_user.period != 'null':
+        period = parser_date(ban_user.period)
+    else:
+        period = None
+
+    await db['web_server_user'].update_one({'username': ban_user.user}, {
+        '$set': {
+            'ban_period': period
+        }
+    })
+
+    if (updated_user := await db['web_server_user'].find_one({'username': ban_user.user})) is not None:
+        return updated_user
+    return HTTPException(status_code=404, detail='User not found')
+
+
+@app.post("/researches", response_model=UserLimitOut)
+async def limit_researches(max_res: LimitResearches):
+    await db['web_server_user'].update_one({'username': max_res.user}, {
+        '$set': {
+            'max_research': max_res.limit
+        }
+    })
+    if (updated_user := await db['web_server_user'].find_one({'username': max_res.user})) is not None:
+        return updated_user
+    return HTTPException(status_code=404, detail='User not found')
+
+
+@app.post("/toggle", response_model=ToggleOut)
+async def toggle_website(toggle: Toggle):
+    if toggle.state == 'disable':
+        state = False
+    elif toggle.state == 'enable':
+        state = True
+    if toggle.web_site == 'goldbet':
+        await db['web_server_settings'].update_one({}, {
+            '$set': {
+                'goldbet_research': state
+            }
+        })
+    else:
+        await db['web_server_settings'].update_one({}, {
+            '$set': {
+                'bwin_research': state
+            }
+        })
+
+    if (updated_setting := await db['web_server_settings'].find_one()) is not None:
+        return updated_setting
+    return HTTPException(status_code=404, detail='Setting not found')
--- models.py
+++ models.py
@@ -1,3 +1,7 @@
+import datetime
+from typing import Optional
+
+from bson import ObjectId
 from pydantic import BaseModel
 
 
@@ -10,3 +14,44 @@ class BetData(BaseModel):
     gol: str
     over: str
     under: str
+
+class UserBanOut(BaseModel):
+    username: str
+    ban_period: Optional[datetime.datetime]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+
+
+class BanUser(BaseModel):
+    user: str
+    period: str
+
+
+class LimitResearches(BaseModel):
+    user: str
+    limit: int
+
+
+class UserLimitOut(BaseModel):
+    username: str
+    max_research: int
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+
+
+class Toggle(BaseModel):
+    state: str
+    web_site: str
+
+
+class ToggleOut(BaseModel):
+    goldbet_research: bool
+    bwin_research: bool
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
